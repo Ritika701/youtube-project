27bd1f6eb8be936d3cc9ca02ffd5660c700d9b76
Ritika Pathak <ritika8949@gmail.com>
2023-11-05 13:08:22 +0530

    Configured tailwindcss and created structure of the project

---
+++ src/components/Body.js
@@ -0,0 +1,26 @@
+
+
+
+
+import React from 'react'
+import MainContainer from './MainContainer'
+import Sidebar from './Sidebar'
+
+const Body = () => {
+    return (
+        <div className='flex'>
+            <Sidebar />
+            <MainContainer />
+        </div>
+    )
+}
+
+export default Body
+
+
+
+
+
+
+
+
